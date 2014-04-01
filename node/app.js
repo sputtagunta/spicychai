423126bfd5dfe7c55f3ca02a8f1758c2fbf8c0e6
Sravan Puttagunta <sravan.puttagunta@gmail.com>
2014-04-01 05:40:51 +0000

    adding changes to app.js

--- node/app.js
+++ node/app.js
@@ -1,12 +1,27 @@
-var app = require('http').createServer(handler).listen(80);
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('/home/ubuntu/spicychai_cert/spicychai.key'),
+  cert: fs.readFileSync('/home/ubuntu/spicychai_cert/2b6910ed4a20d0.crt')
+};
+
+var app = https.createServer(options, handler).listen(9090);
 var io = require('socket.io').listen(app);
+
+io.set('log level', 1);
 
 function handler(req,res){
     console.log(req.url);
     res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello Node\n You are really really awesome!');
+    res.end('');
 }
 
 io.sockets.on('connection',function(socket){
-    
+    console.log(socket.id);
+    socket.on("send_msg", function(data) {
+        console.log("received msg: " + data);
+        socket.broadcast.emit("msg_" + data["path"], data["msg"]);
+    });
 });
+
